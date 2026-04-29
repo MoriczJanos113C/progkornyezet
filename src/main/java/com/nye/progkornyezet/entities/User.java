d9069c4a02401a48b7f506cd909b94c4f12660a5
MoriczJanos113C <moriczj10@gmail.com>
2026-04-29 09:13:23 +0200

    Entities for user, and order.

---
+++ src/main/java/com/nye/progkornyezet/entities/User.java
@@ -0,0 +1,23 @@
+package com.nye.progkornyezet.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+}
