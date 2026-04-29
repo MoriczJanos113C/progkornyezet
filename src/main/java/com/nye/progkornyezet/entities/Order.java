855032ebd14eb88806ef8014e741eb67daeef377
MoriczJanos113C <moriczj10@gmail.com>
2026-04-29 15:20:40 +0200

    Order entity class checkstyle is done.

--- src/main/java/com/nye/progkornyezet/entities/Order.java
+++ src/main/java/com/nye/progkornyezet/entities/Order.java
@@ -1,8 +1,21 @@
 package com.nye.progkornyezet.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Entity representing an order.
+ */
 @Entity
 @Getter
 @Setter
@@ -11,13 +24,26 @@ import lombok.*;
 @Table(name = "orders")
 public class Order {
 
+    /**
+     * Unique identifier.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Product name.
+     */
     private String product;
+
+    /**
+     * Product quantity.
+     */
     private int quantity;
 
+    /**
+     * User who placed the order.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
