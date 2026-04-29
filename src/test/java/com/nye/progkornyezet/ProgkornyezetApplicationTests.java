7d881f62f3013f101d7b50fbd9f17ed55fbed288
MoriczJanos113C <moriczj10@gmail.com>
2026-04-29 08:20:48 +0200

    project creation

---
+++ src/test/java/com/nye/progkornyezet/ProgkornyezetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nye.progkornyezet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgkornyezetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
