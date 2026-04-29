5a54d8059ad1bc610eb81f198bcf5c21806a4d26
MoriczJanos113C <moriczj10@gmail.com>
2026-04-29 15:19:54 +0200

    User entity class checkstyle is done, in pom.xml jacoco, and checkstyle plugins added.

--- src/main/java/com/nye/progkornyezet/entities/User.java
+++ src/main/java/com/nye/progkornyezet/entities/User.java
@@ -1,8 +1,18 @@
 package com.nye.progkornyezet.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -14,13 +24,26 @@ import java.util.List;
 @Table(name = "users")
 public class User {
 
+    /**
+     * Unique identifier.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * User's name.
+     */
     private String name;
+
+    /**
+     * User's email address.
+     */
     private String email;
 
+    /**
+     * Orders for to the user.
+     */
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonIgnore
     private List<Order> orders;
