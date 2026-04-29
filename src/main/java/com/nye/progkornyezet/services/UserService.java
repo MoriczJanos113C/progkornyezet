6fc1d44e2ef4358b023623f6d03c4f7f1c6a9d72
MoriczJanos113C <moriczj10@gmail.com>
2026-04-29 14:14:46 +0200

    User service is done.

---
+++ src/main/java/com/nye/progkornyezet/services/UserService.java
@@ -0,0 +1,31 @@
+package com.nye.progkornyezet.services;
+
+import com.nye.progkornyezet.entities.User;
+import com.nye.progkornyezet.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User getById(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
