f6e050dd23dc79c52a19ff2bfb20075e4fbb86d1
MoriczJanos113C <moriczj10@gmail.com>
2026-05-04 09:01:10 +0200

    Controllers checkstyle is done, package-info added.

--- src/main/java/com/nye/progkornyezet/controllers/OrderController.java
+++ src/main/java/com/nye/progkornyezet/controllers/OrderController.java
@@ -3,29 +3,54 @@ package com.nye.progkornyezet.controllers;
 import com.nye.progkornyezet.entities.Order;
 import com.nye.progkornyezet.services.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * REST controller for managing orders.
+ */
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
+    /**
+     * Order service instance.
+     */
     private final OrderService orderService;
 
+    /**
+     * Returns all orders.
+     * @return list of all orders
+     */
     @GetMapping
     public List<Order> getAll() {
         return orderService.getAll();
     }
 
+    /**
+     * Creates a new order.
+     * @param order order to create
+     * @return created order
+     */
     @PostMapping("/create")
-    public Order create(@RequestBody Order order) {
+    public Order create(final @RequestBody Order order) {
         return orderService.save(order);
     }
 
+    /**
+     * Deletes an order by ID.
+     * @param id order ID
+     */
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Long id) {
+    public void delete(final @PathVariable Long id) {
         orderService.delete(id);
     }
 }
--- src/main/java/com/nye/progkornyezet/controllers/UserController.java
+++ src/main/java/com/nye/progkornyezet/controllers/UserController.java
@@ -3,40 +3,80 @@ package com.nye.progkornyezet.controllers;
 import com.nye.progkornyezet.entities.User;
 import com.nye.progkornyezet.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * REST controller for User operations.
+ */
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
+    /**
+     * User service instance.
+     */
     private final UserService userService;
 
+    /**
+     * Returns all users.
+     * @return list of users
+     */
     @GetMapping
     public List<User> getAll() {
         return userService.getAll();
     }
 
+    /**
+     * Returns a user by ID.
+     * @param id user ID
+     * @return found user
+     */
     @GetMapping("/oneUser/{id}")
-    public User getById(@PathVariable Long id) {
+    public User getById(final @PathVariable Long id) {
         return userService.getById(id);
     }
 
+    /**
+     * Creates a new user.
+     * @param user user to create
+     * @return created user
+     */
     @PostMapping("/create")
-    public User create(@RequestBody User user) {
+    public User create(final @RequestBody User user) {
         return userService.save(user);
     }
 
+    /**
+     * Updates a user.
+     * @param id user ID
+     * @param user updated user data
+     * @return updated user
+     */
     @PutMapping("/update/{id}")
-    public User update(@PathVariable Long id, @RequestBody User user) {
+    public User update(
+            final @PathVariable Long id,
+            final @RequestBody User user
+    ) {
         user.setId(id);
         return userService.save(user);
     }
 
+    /**
+     * Deletes a user by ID.
+     * @param id user ID
+     */
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Long id) {
+    public void delete(final @PathVariable Long id) {
         userService.delete(id);
     }
 }
